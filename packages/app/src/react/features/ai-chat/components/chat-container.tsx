02edbd3e66cd769a5014467786018113f16215fe
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-09-05 01:28:25 +0600

    Update layout of ChatContainer and ScrollToBottomButton components for improved styling
    - Added 'relative' class to ChatContainer for better positioning.
    - Restricted width of ScrollToBottomButton to 'max-w-4xl' for consistent layout across different screen sizes.

--- packages/app/src/react/features/ai-chat/components/chat-container.tsx
+++ packages/app/src/react/features/ai-chat/components/chat-container.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react';
 
 type IChildren = { children: ReactNode };
 export const ChatContainer: FC<IChildren> = ({ children }) => (
-  <div className="w-full h-full max-w-4xl flex flex-col items-center justify-end px-4 md:px-0 ph-no-capture mx-auto">
+  <div className="w-full h-full max-w-4xl flex flex-col items-center justify-end px-4 md:px-0 ph-no-capture mx-auto relative">
     {children}
   </div>
 );
