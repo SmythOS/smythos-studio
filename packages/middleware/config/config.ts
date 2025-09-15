9c7c1b5af48dc0f42e40c8dca33d6de32f96e532
Ahmed Essam <domysalem10@gmail.com>
2025-09-15 15:18:56 -0700

    env expand feat & config changes

--- packages/middleware/config/config.ts
+++ packages/middleware/config/config.ts
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import expandEnv from 'dotenv-expand';
 import dotenvFlow from 'dotenv-flow';
 import os from 'os';
 import path from 'path';
 import { name, version } from '../package.json';
 import { getDirname } from '../src/utils/general';
 
-dotenvFlow.config({
-  files: ['../../.env', '../.env'],
-});
+expandEnv.expand(
+  dotenvFlow.config({
+    files: ['../../.env', '../.env'],
+  }),
+);
 
 // const rootPath = require.main?.path;
 const rootPath = path.join(getDirname(), '..');
@@ -30,6 +33,11 @@ export const config = {
     env: process.env.NODE_ENV,
     port: process.env.MIDDLEWARE_API_PORT,
 
+    DATABASE_PROVIDER: process.env.DATABASE_PROVIDER,
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_NAME: process.env.DATABASE_NAME,
     DATABASE_URL: process.env.DATABASE_URL,
     VAULT_FILE_PATH: path.join(dataPath, 'vault.json'),
   },
