687c006bd208728b39c7eb1be2f87e1f8baee98d
Forhad Hosain <forhad@smythos.com>
2025-12-30 22:29:55 +0600

    provide instruction for country code

--- packages/app/src/builder-ui/components/GenAILLM.class.ts
+++ packages/app/src/builder-ui/components/GenAILLM.class.ts
@@ -977,7 +977,7 @@ export class GenAILLM extends Component {
         datalistId: 'country-datalist',
         attributes: {
           ...openAIAttributes,
-          placeholder: 'US',
+          placeholder: 'Type country name or code (e.g., United States or US) to search.',
           'data-template-vars': '{"enabled": true, "singleOnly": true}',
           'data-template-excluded-vars': 'Attachment',
         },
@@ -1009,7 +1009,7 @@ export class GenAILLM extends Component {
         datalistId: 'timezone-datalist',
         attributes: {
           ...openAIAttributes,
-          placeholder: 'America/Chicago',
+          placeholder: 'Type to search timezones (e.g., America/Chicago)',
           'data-template-vars': '{"enabled": true, "singleOnly": true}',
           'data-template-excluded-vars': 'Attachment',
         },
@@ -1171,7 +1171,7 @@ export class GenAILLM extends Component {
         datalistId: 'country-datalist',
         attributes: {
           ...xAIAttributes,
-          placeholder: 'US',
+          placeholder: 'Type country name or code (e.g., United States or US) to search.',
           'data-template-vars': '{"enabled": true, "singleOnly": true}',
           'data-template-excluded-vars': 'Attachment',
         },
